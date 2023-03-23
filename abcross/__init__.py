9777709bab7957206b3536c5ac2cd5a07c1db77a
Tianhao Chai <cth451@gmail.com>
2023-03-23 18:50:42 -0400

    basic cli usage

--- abcross/__init__.py
+++ abcross/__init__.py
@@ -1,4 +1,3 @@
-from .cli import main
 from .common import Architecture
 from .distribution import get_tarball, get_manifest, get_release_tarball_info, Variant
 from .io import extract_tarball
--- abcross/cli.py
+++ abcross/cli.py
@@ -1,2 +1,54 @@
+import argparse
+import sys
+
+from .common import Architecture
+from .distribution import Variant
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description="AOSC OS cross-compiling sysroot manager")
+    parser.add_argument("-a", "--arch",
+                        required=True,
+                        help="Work on the sysroot for specified architecture",
+                        type=Architecture
+                        )
+    subparsers = parser.add_subparsers(
+        title="commands",
+        dest="command",
+        help="Task to do. \n"
+             "Use \"install\" to download a new sysroot.\n"
+             "Use \"enter\" to launch a shell in the sysroot for maintenance."
+    )
+    subparser_install = subparsers.add_parser("install", help="Download and deploy a cross compile sysroot.")
+    subparser_install.add_argument(
+        "-m", "--mirror",
+        help="Use specified mirror. Default to \"https://repo.aosc.io/\"",
+        type=str,
+        default="https://repo.aosc.io/"
+    )
+    subparser_install.add_argument(
+        "-v", "--variant",
+        help="Download tarball of specified variant. Default to Buildkit.",
+        type=Variant,
+        default=Variant.BUILDKIT
+    )
+    subparser_install.add_argument(
+        "-f", "--force",
+        help="Force overwriting a sysroot directory with existing data.",
+        action="store_true"
+    )
+    subparser_enter = subparsers.add_parser("enter", help="Start an interactive shell or program in the sysroot.")
+    subparser_enter.add_argument(
+        "argv",
+        nargs='*',
+        help="Space separated argv to spawn in the container. Default to \"/bin/bash\".",
+        default=["/bin/bash"]
+    )
+
+    args = parser.parse_args()
+    print(args)
+    sys.exit(5)
+
+
+if __name__ == "__main__":
+    main()
